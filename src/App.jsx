6bc883d4db4822b511cecd58f0de08efa7361529
Triet Tran <anhtriet172@gmail.com>
2022-04-21 22:06:54 +0700

    adding categories & products page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Home } from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Announcement from "./components/Announcement";
 import Slider from "./components/Slider";
+import Categories from "./components/Categories";
+import Products from "./components/Products";
 const App = () => {
   return (
     <>
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
+      <Products />
     </>
   );
 };
